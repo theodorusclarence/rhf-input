39bfde81691016d1de1c6a4df7f8c2eb78e0d6fe
Theodorus Clarence <theodorusclarence@gmail.com>
2021-07-09 15:08:17 +0700

    feat: add HashLink

--- components/HashLink.jsx
+++ components/HashLink.jsx
@@ -1,12 +1,12 @@
 import { classNames } from '@/lib/helper';
 import UnstyledLink from './UnstyledLink';
 
-export default function CustomLink({ children, className = '', ...rest }) {
+export default function HashLink({ className, children, ...rest }) {
   return (
     <UnstyledLink
       {...rest}
       className={classNames(
-        'inline-flex items-center font-bold hover:text-primary-500 focus:hover:text-primary-500 animated-underline',
+        'inline-block font-bold focus:outline-none hash-anchor',
         className
       )}
     >
