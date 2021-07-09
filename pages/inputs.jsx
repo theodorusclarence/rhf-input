eae814a20cd14661e00eac90703a9b5e8733bdeb
Theodorus Clarence <theodorusclarence@gmail.com>
2021-07-09 20:40:42 +0700

    refactor: put each inputs to own sections

--- pages/inputs.jsx
+++ pages/inputs.jsx
@@ -40,213 +40,11 @@ export default function InputsPage() {
                 onSubmit={handleSubmit(onSubmit)}
                 className='max-w-sm mt-12 space-y-12'
               >
-                <section id='text-input' className='space-y-4'>
-                  <header className='space-y-1'>
-                    <div>
-                      <HashLink href='#text-input'>
-                        <h2>Text Input</h2>
-                      </HashLink>
-                    </div>
-                    <div>
-                      <CustomLink
-                        href='https://github.com/theodorusclarence/rhf-input/blob/main/components/Input.jsx'
-                        className='inline-block font-medium text-gray-700'
-                      >
-                        Component source code
-                      </CustomLink>
-                    </div>
-                  </header>
-
-                  <div className='space-y-2' id='text-input-normal'>
-                    <HashLink href='#normal-input'>
-                      <h3 className='text-lg'>Normal Text Input</h3>
-                    </HashLink>
-                    <Input id='input' label='Label' helperText='Helper text' />
-                  </div>
-                  <div className='space-y-2' id='text-input-required'>
-                    <HashLink href='#input-required'>
-                      <h3 className='text-lg'>
-                        Text Input with required validation (onTouched)
-                      </h3>
-                    </HashLink>
-                    <Input
-                      id='input2'
-                      label='Label'
-                      helperText='Helper text'
-                      validation={{ required: 'Custom error message' }}
-                    />
-                  </div>
-                  <div className='space-y-2' id='text-input-readonly'>
-                    <HashLink href='#text-input-readonly'>
-                      <h3 className='text-lg'>Text Input Read Only</h3>
-                    </HashLink>
-                    <Input
-                      id='input3'
-                      label='Label'
-                      helperText='Helper text'
-                      readOnly
-                      defaultValue='Default Value'
-                    />
-                  </div>
-                </section>
-
+                <InputSection />
                 <hr />
-
-                <section id='text-area' className='space-y-4'>
-                  <header className='space-y-1'>
-                    <div>
-                      <HashLink href='#text-area'>
-                        <h2>Text Area</h2>
-                      </HashLink>
-                    </div>
-                    <div>
-                      <CustomLink
-                        href='https://github.com/theodorusclarence/rhf-input/blob/main/components/TextArea.jsx'
-                        className='inline-block font-medium text-gray-700'
-                      >
-                        Component source code
-                      </CustomLink>
-                    </div>
-                  </header>
-                  <div className='space-y-2' id='default-textarea'>
-                    <HashLink href='#default-textarea'>
-                      <h3 className='text-lg'>Default Text Area</h3>
-                    </HashLink>
-                    <TextArea
-                      id='textarea'
-                      label='Label'
-                      helperText='Helper text'
-                    />
-                  </div>
-
-                  <div className='space-y-2' id='textarea-required'>
-                    <HashLink href='#textarea-required'>
-                      <h3 className='text-lg'>
-                        Text Area with required validation (onTouched)
-                      </h3>
-                    </HashLink>
-                    <TextArea
-                      id='textarea2'
-                      label='Label'
-                      helperText='Helper text'
-                      validation={{ required: 'Custom error message' }}
-                    />
-                  </div>
-
-                  <div className='space-y-2' id='textarea-readonly'>
-                    <HashLink href='#textarea-readonly'>
-                      <h3 className='text-lg'>Text Area Read Only</h3>
-                    </HashLink>
-                    <TextArea
-                      id='textarea3'
-                      label='Label'
-                      helperText='Helper text'
-                      readOnly
-                      defaultValue='Default Value'
-                    />
-                  </div>
-                </section>
-
+                <TextAreaSection />
                 <hr />
-
-                <section id='date-picker' className='space-y-4'>
-                  <header className='space-y-1'>
-                    <div>
-                      <HashLink href='#date-picker'>
-                        <h2>DatePicker</h2>
-                      </HashLink>
-                    </div>
-                    <div>
-                      <CustomLink
-                        href='https://github.com/theodorusclarence/rhf-input/blob/main/components/DatePicker.jsx'
-                        className='inline-block font-medium text-gray-700'
-                      >
-                        Component source code
-                      </CustomLink>
-                    </div>
-                  </header>
-
-                  <div className='space-y-2' id='default-datepick'>
-                    <HashLink href='#default-datepick'>
-                      <h3 className='text-lg'>Default DatePicker</h3>
-                    </HashLink>
-                    <DatePicker
-                      id='datepicker'
-                      label='Label'
-                      placeholder='Placeholder'
-                      helperText='Helper text'
-                    />
-                  </div>
-
-                  <div className='space-y-2' id='datepicker-defaultyear'>
-                    <HashLink href='#datepicker-defaultyear'>
-                      <h3 className='text-lg'>
-                        DatePicker with defaultYear to 2001
-                      </h3>
-                    </HashLink>
-                    <DatePicker
-                      id='datepicker2'
-                      label='Label'
-                      helperText='Helper text'
-                      defaultYear='2001'
-                    />
-                  </div>
-
-                  <div className='space-y-2' id='datepicker-defaultmonth'>
-                    <HashLink href='#datepicker-defaultmonth'>
-                      <h3 className='text-lg'>
-                        DatePicker with defaultMonth to May
-                      </h3>
-                    </HashLink>
-                    {/* Month starts from 0 */}
-                    <DatePicker
-                      id='datepicker2'
-                      label='Label'
-                      helperText='Helper text'
-                      defaultMonth='4'
-                    />
-                  </div>
-
-                  <div className='space-y-2' id='datepicker-locale'>
-                    <HashLink href='#datepicker-locale'>
-                      <h3 className='text-lg'>DatePicker with ID Locale</h3>
-                    </HashLink>
-                    {/* import id from date-fns/locale/id */}
-                    <DatePicker
-                      id='datepicker3'
-                      label='Label'
-                      helperText='Helper text'
-                      locale={id}
-                    />
-                  </div>
-
-                  <div className='space-y-2' id='datepicker-required'>
-                    <HashLink href='#datepicker-required'>
-                      <h3 className='text-lg'>
-                        DatePicker with required validation
-                      </h3>
-                    </HashLink>
-                    <DatePicker
-                      id='datepicker4'
-                      label='Label'
-                      helperText='Helper text'
-                      validation={{ required: 'Custom error message' }}
-                    />
-                  </div>
-
-                  <div className='space-y-2' id='datepicker-readonly'>
-                    <HashLink href='#datepicker-readonly'>
-                      <h3 className='text-lg'>DatePicker Read Only</h3>
-                    </HashLink>
-                    <DatePicker
-                      id='datepicker5'
-                      label='Label'
-                      helperText='Helper text'
-                      defaultValue={new Date()}
-                      readOnly
-                    />
-                  </div>
-                </section>
+                <DatePickerSection />
 
                 <Button type='submit'>Submit Button (check console)</Button>
               </form>
@@ -255,5 +53,211 @@ export default function InputsPage() {
         </div>
       </main>
     </>
+  );
+}
+
+function InputSection() {
+  return (
+    <section id='text-input' className='space-y-4'>
+      <header className='space-y-1'>
+        <div>
+          <HashLink href='#text-input'>
+            <h2>Text Input</h2>
+          </HashLink>
+        </div>
+        <div>
+          <CustomLink
+            href='https://github.com/theodorusclarence/rhf-input/blob/main/components/Input.jsx'
+            className='inline-block font-medium text-gray-700'
+          >
+            Component source code
+          </CustomLink>
+        </div>
+      </header>
+
+      <div className='space-y-2' id='text-input-normal'>
+        <HashLink href='#normal-input'>
+          <h3 className='text-lg'>Normal Text Input</h3>
+        </HashLink>
+        <Input id='input' label='Label' helperText='Helper text' />
+      </div>
+      <div className='space-y-2' id='text-input-required'>
+        <HashLink href='#input-required'>
+          <h3 className='text-lg'>
+            Text Input with required validation (onTouched)
+          </h3>
+        </HashLink>
+        <Input
+          id='input2'
+          label='Label'
+          helperText='Helper text'
+          validation={{ required: 'Custom error message' }}
+        />
+      </div>
+      <div className='space-y-2' id='text-input-readonly'>
+        <HashLink href='#text-input-readonly'>
+          <h3 className='text-lg'>Text Input Read Only</h3>
+        </HashLink>
+        <Input
+          id='input3'
+          label='Label'
+          helperText='Helper text'
+          readOnly
+          defaultValue='Default Value'
+        />
+      </div>
+    </section>
+  );
+}
+
+function TextAreaSection() {
+  return (
+    <section id='text-area' className='space-y-4'>
+      <header className='space-y-1'>
+        <div>
+          <HashLink href='#text-area'>
+            <h2>Text Area</h2>
+          </HashLink>
+        </div>
+        <div>
+          <CustomLink
+            href='https://github.com/theodorusclarence/rhf-input/blob/main/components/TextArea.jsx'
+            className='inline-block font-medium text-gray-700'
+          >
+            Component source code
+          </CustomLink>
+        </div>
+      </header>
+      <div className='space-y-2' id='default-textarea'>
+        <HashLink href='#default-textarea'>
+          <h3 className='text-lg'>Default Text Area</h3>
+        </HashLink>
+        <TextArea id='textarea' label='Label' helperText='Helper text' />
+      </div>
+
+      <div className='space-y-2' id='textarea-required'>
+        <HashLink href='#textarea-required'>
+          <h3 className='text-lg'>
+            Text Area with required validation (onTouched)
+          </h3>
+        </HashLink>
+        <TextArea
+          id='textarea2'
+          label='Label'
+          helperText='Helper text'
+          validation={{ required: 'Custom error message' }}
+        />
+      </div>
+
+      <div className='space-y-2' id='textarea-readonly'>
+        <HashLink href='#textarea-readonly'>
+          <h3 className='text-lg'>Text Area Read Only</h3>
+        </HashLink>
+        <TextArea
+          id='textarea3'
+          label='Label'
+          helperText='Helper text'
+          readOnly
+          defaultValue='Default Value'
+        />
+      </div>
+    </section>
+  );
+}
+
+function DatePickerSection() {
+  return (
+    <section id='date-picker' className='space-y-4'>
+      <header className='space-y-1'>
+        <div>
+          <HashLink href='#date-picker'>
+            <h2>DatePicker</h2>
+          </HashLink>
+        </div>
+        <div>
+          <CustomLink
+            href='https://github.com/theodorusclarence/rhf-input/blob/main/components/DatePicker.jsx'
+            className='inline-block font-medium text-gray-700'
+          >
+            Component source code
+          </CustomLink>
+        </div>
+      </header>
+
+      <div className='space-y-2' id='default-datepick'>
+        <HashLink href='#default-datepick'>
+          <h3 className='text-lg'>Default DatePicker</h3>
+        </HashLink>
+        <DatePicker
+          id='datepicker'
+          label='Label'
+          placeholder='Placeholder'
+          helperText='Helper text'
+        />
+      </div>
+
+      <div className='space-y-2' id='datepicker-defaultyear'>
+        <HashLink href='#datepicker-defaultyear'>
+          <h3 className='text-lg'>DatePicker with defaultYear to 2001</h3>
+        </HashLink>
+        <DatePicker
+          id='datepicker2'
+          label='Label'
+          helperText='Helper text'
+          defaultYear='2001'
+        />
+      </div>
+
+      <div className='space-y-2' id='datepicker-defaultmonth'>
+        <HashLink href='#datepicker-defaultmonth'>
+          <h3 className='text-lg'>DatePicker with defaultMonth to May</h3>
+        </HashLink>
+        {/* Month starts from 0 */}
+        <DatePicker
+          id='datepicker2'
+          label='Label'
+          helperText='Helper text'
+          defaultMonth='4'
+        />
+      </div>
+
+      <div className='space-y-2' id='datepicker-locale'>
+        <HashLink href='#datepicker-locale'>
+          <h3 className='text-lg'>DatePicker with ID Locale</h3>
+        </HashLink>
+        {/* import id from date-fns/locale/id */}
+        <DatePicker
+          id='datepicker3'
+          label='Label'
+          helperText='Helper text'
+          locale={id}
+        />
+      </div>
+
+      <div className='space-y-2' id='datepicker-required'>
+        <HashLink href='#datepicker-required'>
+          <h3 className='text-lg'>DatePicker with required validation</h3>
+        </HashLink>
+        <DatePicker
+          id='datepicker4'
+          label='Label'
+          helperText='Helper text'
+          validation={{ required: 'Custom error message' }}
+        />
+      </div>
+
+      <div className='space-y-2' id='datepicker-readonly'>
+        <HashLink href='#datepicker-readonly'>
+          <h3 className='text-lg'>DatePicker Read Only</h3>
+        </HashLink>
+        <DatePicker
+          id='datepicker5'
+          label='Label'
+          helperText='Helper text'
+          defaultValue={new Date()}
+          readOnly
+        />
+      </div>
+    </section>
   );
 }
